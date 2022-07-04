ddd2607a9f12eaf290b5b631ab57ae27f8592036
Aleksey <epsilonalexman@gmail.com>
2022-07-04 15:21:28 +0300

    second commit

---
+++ js/app1.js
@@ -0,0 +1,128 @@
+// получанием input'ов из html
+const emailInput = document.getElementById('email__input')
+const nameInput = document.querySelector('#name__input')
+const lastNameInput = document.querySelector('#last-name__input')
+const phoneInput = document.querySelector('#phone__input')
+// Счётчик отправки форм
+let sendCounter = 0
+
+document.addEventListener('submit', function (event) {
+    // Валидация email и проверка заполненности полей name и lastname
+    if (emailTest(emailInput) && !emailInput.nextElementSibling) {
+        emailInput.parentElement.insertAdjacentHTML(
+            'beforeend',
+            `<div class="main-form__error">
+                Insert right e-mail
+            </div> `)
+    } else {
+            event.preventDefault()
+    }  
+    if (!lastNameInput.value && !lastNameInput.nextElementSibling) {
+        lastNameInput.parentElement.insertAdjacentHTML(
+            'beforeend',
+            `<div class="form__lastname-empty">
+                It can not be empty
+            </div>`
+        )
+    } else {
+        event.preventDefault()
+    }
+    if (!nameInput.value && !nameInput.nextElementSibling) {
+        nameInput.parentElement.insertAdjacentHTML(
+            'beforeend',
+            `<div class="form__name-empty">
+                    It can not be empty
+                 </div>`
+        )
+    }else {
+        event.preventDefault()
+    }
+    // Если поля поля пустые, то форма не отправится
+    const objectData = {}
+    if (nameInput.value === '' || lastNameInput.value === '' || emailInput.value === '' || phoneInput.value === '') {
+        event.preventDefault()
+        localStorage.removeItem(`personData${sendCounter+1}`)
+    }else {
+        // если всё заполнено, то просто отправляем форму и поля делаем пустыми
+        nameInput.value = ''
+        lastNameInput.value = ''
+        emailInput.value = ''
+        phoneInput.value = ''
+        // увеличиваем счётчик отправок
+        sendCounter += 1
+        // заполняем объект данными из localStorage 
+        objectData.name = localStorage.getItem('name')
+        objectData.lastname = localStorage.getItem('lastname')
+        objectData.email = localStorage.getItem('email')
+        objectData.phone = localStorage.getItem('phone')
+        objectData.id = sendCounter
+        // заносим в localStorage 
+        localStorage.setItem('sendCounter', sendCounter)
+        localStorage.setItem(`personData${sendCounter}`, JSON.stringify(objectData))
+        // используется для "связи" двух js файлов
+        localStorage.setItem('key', 'key')
+    }
+    // очищаем поля в localStorage      
+    localStorage.removeItem('name')
+    localStorage.removeItem('lastname')
+    localStorage.removeItem('email')
+    localStorage.removeItem('phone')
+})
+
+sendCounter = Number(localStorage.getItem('sendCounter'))
+
+// удаление предупреждающего объявления под email и name и lastname
+emailInput.addEventListener('focus', function (e) {
+    if (emailInput.nextElementSibling) {
+        emailInput.nextElementSibling.remove()
+    }
+})
+nameInput.addEventListener('focus', function (e) {
+    if (nameInput.nextElementSibling) {
+        nameInput.nextElementSibling.remove()
+    }
+})
+lastNameInput.addEventListener('focus', function (e) {
+    if (lastNameInput.nextElementSibling) {
+        lastNameInput.nextElementSibling.remove()
+    }
+})
+
+// функция проверки email
+function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
+
+
+// слежка за изменениями в input'ах
+document.addEventListener('input', e => {
+    if (e.target.closest = nameInput) {
+        localStorage.setItem('name', nameInput.value)
+    }
+    if (e.target.closest = lastNameInput) {
+        localStorage.setItem('lastname', lastNameInput.value)
+    }
+    if (e.target.closest = emailInput) {
+        localStorage.setItem('email', emailInput.value)
+    }
+    if (e.target.closest = phoneInput) {
+        localStorage.setItem('phone', phoneInput.value)
+    }
+})
+
+// после перезгрузки заполенение полей формы данными из localStorage
+document.addEventListener('DOMContentLoaded', () => {
+    if (!nameInput.value) {
+        nameInput.value = localStorage.getItem('name')
+    }
+    if (!lastNameInput.value) {
+        lastNameInput.value = localStorage.getItem('lastname')
+    }
+    if (!emailInput.value) {
+        emailInput.value = localStorage.getItem('email')
+    }
+    if (!phoneInput.value) {
+        phoneInput.value = localStorage.getItem('phone')
+    }
+})
+
---
+++ js/app2.js
@@ -0,0 +1,106 @@
+const main = document.querySelector('.main')
+const mainDeleteAll = document.querySelector('.main__delete-all')
+
+window.addEventListener('storage', () => {
+    if (localStorage.getItem('key')) {
+
+        localStorage.removeItem('key')
+
+        // создание карточки
+        makeCard()
+
+        // получаем элемент с данными, что заполнить карточку
+        const sendCounterHistory = Number(localStorage.getItem('sendCounter'))
+
+        // получаем id из объекта в localStorage и присваиваем это id в html
+        let idForMainForm = Number(JSON.parse(localStorage.getItem(`personData${sendCounterHistory}`)).id)
+        const mainForm = document.querySelector('.main__form')
+        mainForm.id = `${idForMainForm}`
+
+        // получаем элементы из html и заполняем их
+        const namePut = document.querySelector('.name__put')
+        const lastnamePut = document.querySelector('.lastname__put')
+        const emailPut = document.querySelector('.email__put')
+        const phonePut = document.querySelector('.phone__put')
+        const personData = JSON.parse(localStorage.getItem(`personData${sendCounterHistory}`))
+        namePut.innerHTML = personData.name
+        lastnamePut.innerHTML = personData.lastname
+        emailPut.innerHTML = personData.email
+        phonePut.innerHTML = personData.phone
+    }
+})
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sendCounterHistory = Number(localStorage.getItem('sendCounter'))
+    // условие >= 2 означает то, что если в localStorage один элемент sendCounter, то никаких действия выполнять не нужно
+    if (localStorage.length >= 2) {
+        for (let i = 1; i <= sendCounterHistory;) {
+            // если в хранилище нет объекта с номером i, то просто идём дальше
+            if (!localStorage.getItem(`personData${i}`)) {
+                i += 1
+            }else {
+                // создание карточки
+                makeCard()
+                // получаем элемент с данными, чтобы заполнить карточку
+                const personData = JSON.parse(localStorage.getItem(`personData${i}`))
+
+                // получаем id из объекта в localStorage и присваиваем это id в html
+                const mainForm = document.querySelector('.main__form')
+                let idForMainForm = Number(JSON.parse(localStorage.getItem(`personData${i}`)).id)
+                mainForm.id = `${idForMainForm}`
+
+                // получаем элементы из html и заполняем их
+                const namePut = document.querySelector('.name__put')
+                const lastnamePut = document.querySelector('.lastname__put')
+                const emailPut = document.querySelector('.email__put')
+                const phonePut = document.querySelector('.phone__put')
+                namePut.innerHTML = personData.name
+                lastnamePut.innerHTML = personData.lastname
+                emailPut.innerHTML = personData.email
+                phonePut.innerHTML = personData.phone
+                i += 1
+            } 
+        }
+    }
+})
+
+// Кнопка для удаления формы и кнопка для удаления всех форм
+document.addEventListener('click', (e) => {
+    const deleteButton = document.querySelector('.form__button')
+    if (e.target.closest('.form__button')) {
+        // получение id из элемента из localStorage, это нужно, чтобы удалить правильную карточку 
+        localStorage.removeItem(`personData${deleteButton.parentElement.id}`)
+        deleteButton.parentElement.remove()
+    }
+    if (e.target.closest('.main__delete-all')) {
+        localStorage.clear()
+        mainDeleteAll.nextElementSibling.remove()
+    }
+})
+
+
+// функция для добавления карточки с данными
+function makeCard () {
+    mainDeleteAll.nextElementSibling.insertAdjacentHTML('afterbegin',
+    `<div class="main__form">
+        <div class="form__name form__item">
+            <h3>Name</h3>
+            <p class="name__put put"></p>
+        </div>
+        <div class="form__last-name form__item">
+            <h3>Lastname</h3>
+            <p class="lastname__put put"></p>
+        </div>
+        <div class="form__email form__item">
+            <h3>E-mail</h3>
+            <p class="email__put put"></p>
+        </div>
+        <div class="form__phone form__item">
+            <h3>Phone number</h3>
+            <p class="phone__put put"></p>
+        </div>
+        <button class="form__button" type="delete-button">Delete</button>
+    </div>`)
+}
+
